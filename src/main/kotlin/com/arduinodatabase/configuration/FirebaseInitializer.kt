18d82c1db318cb0d02c39b559e450eaba4962c82
jakub <jakubsenyr@gmail.com>
2025-01-27 12:41:30 +0100

    different stream

--- src/main/kotlin/com/arduinodatabase/configuration/FirebaseInitializer.kt
+++ src/main/kotlin/com/arduinodatabase/configuration/FirebaseInitializer.kt
@@ -5,22 +5,22 @@ import com.google.cloud.firestore.Firestore
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.cloud.FirestoreClient
-import java.io.FileInputStream
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.io.InputStream
 
 @Configuration
 class FirebaseConfiguration() {
 
     @Bean
     fun firestore(): Firestore? {
-        val serviceAccount = FileInputStream("src/main/resources/static/serviceAccount.json")
+        val serviceAccountStream: InputStream? = this::class.java.getResourceAsStream("/serviceAccount.json")
         val options = FirebaseOptions.builder()
-            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .setCredentials(GoogleCredentials.fromStream(serviceAccountStream))
             .setProjectId("sample-firebase-ai-app-d2b66")
             .build()
 
-        val firebaseApp = FirebaseApp.initializeApp(options);
-        return FirestoreClient.getFirestore(firebaseApp);
+        val firebaseApp = FirebaseApp.initializeApp(options)
+        return FirestoreClient.getFirestore(firebaseApp)
     }
 }
